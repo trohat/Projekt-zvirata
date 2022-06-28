508a8c069a7d22ac4173b0939c59ccadd3a673b4
Trohat <petr.sed@centrum.cz>
2022-06-28 19:26:12 +0200

    Dědičnost šablon, statika

--- zvirata/informace/urls.py
+++ zvirata/informace/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("seznam/", views.seznam_zvirat, name="seznam"),
-    path("slon/", views.slon),
-    path("zirafa/", views.zirafa),
-    path("<zvire>/", views.detail_zvirete, name="detail"),
-    path("", views.index)
+    path("slon/", views.slon, name="slon"),
+    path("zirafa/", views.zirafa, name="zirafa"),
+    path("detail/<zvire>/", views.detail_zvirete, name="detail"),
+    path("", views.index, name="index")
 ]
